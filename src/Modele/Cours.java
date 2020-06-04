e4548192183c24c5860ef1a05a08d2dd2f159148
TerencePL <36531921+TerencePL@users.noreply.github.com>
2020-06-04 19:52:25 +0200

    Remplissages classe
    Constructeurs, getters,setters pour Cours  et Enseignant

--- src/Modele/Cours.java
+++ src/Modele/Cours.java
@@ -8,4 +8,57 @@ public class Cours {
     private int id;
     private String nom;
 
+    //Constructeur par default
+    public Cours(){
+        id=0;
+        nom=null;      
+    }
+
+    //constructeur surchargé
+    public Cours(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+
+    }
+    
+    /**
+     *Afficher un cours
+     */
+    public void afficher(){
+        System.out.println("Id:" + id);
+        System.out.println("Nom:" + nom);
+        //System.out.println(classe.getId_classe()+" "+classe.getNom());
+        //System.out.println(eleve.getId()+" "+eleve.getNom());
+    } 
+    
+    
+    /*
+    @Override
+    public boolean equals(Object inscription){
+        return inscription instanceof Inscription &&
+                ((Inscription) inscription).id_inscription==this.id_inscription &&
+                ((Inscription) inscription).classe.equals(this.classe)&&
+                ((Inscription) inscription).eleve.equals(this.eleve);
+    } */
+    
+    
+    //Getters 
+    public int getId() {
+        return id;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
+
+    //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    
 }
--- src/Modele/Enseignant.java
+++ src/Modele/Enseignant.java
@@ -5,6 +5,47 @@ import java.sql.SQLException;
 
 public class Enseignant {
     private int id_utilisateur;
-    private String id_cours;
+    private int id_cours;
 
+    
+    //Constructeur par default
+    public Enseignant(){
+        id_utilisateur=0;
+        id_cours=0;      
+    }
+
+    //constructeur surchargé
+    public Enseignant(int id_utilisateur, int id_cours) {
+        this.id_utilisateur = id_utilisateur;
+        this.id_cours = id_cours;
+    }
+    
+    /**
+     *Afficher un cours
+     */
+    public void afficher(){
+        System.out.println("Id_utilisateur:" + id_utilisateur);
+        System.out.println("Id_cours:" + id_cours);
+    } 
+    
+    
+    
+    //Getters 
+    public int getId_utilisateur() {
+        return id_utilisateur;
+    }
+    
+    public String getId_cours() {
+        return id_cours;
+    }
+
+    //Setters
+    public void setId(int id_utilisateur) {
+        this.id_utilisateur = id_utilisateur;
+    }
+    
+    public void setId_cours(int id_cours) {
+        this.id_cours = id_cours;
+    }
+    
 }
